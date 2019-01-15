42c12f2215e32bf0448abfaa4b43191d9ce587b9
Joey <joey.xf@gmail.com>
2019-01-15 11:22:58 +0800

    add solution of p52

--- project-euler/src/lib.rs
+++ project-euler/src/lib.rs
@@ -12,3 +12,4 @@ pub mod p43;
 pub mod p46;
 pub mod p47;
 pub mod p50;
+pub mod p52;
---
+++ project-euler/src/p52.rs
@@ -0,0 +1,46 @@
+pub fn solution_of_p52() -> usize {
+    for i in 1..1000000 {
+        let p2 = 2 * i;
+        let p3 = 3 * i;
+        let p4 = 4 * i;
+        let p5 = 5 * i;
+        let p6 = 6 * i;
+        if is_contains_same_digits(i, p2)
+            && is_contains_same_digits(p3, p4)
+            && is_contains_same_digits(p5, p6)
+            && is_contains_same_digits(i, p3)
+            && is_contains_same_digits(i, p5)
+        {
+            return i;
+        }
+    }
+    return 0;
+}
+
+pub fn is_contains_same_digits(n1: usize, n2: usize) -> bool {
+    let s1 = n1.to_string();
+    let mut v1: Vec<char> = s1.chars().collect();
+    v1.sort();
+    let s2 = n2.to_string();
+    let mut v2: Vec<char> = s2.chars().collect();
+    v2.sort();
+
+    v1 == v2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solution_of_p52() {
+        assert_eq!(142857, solution_of_p52());
+    }
+
+    #[test]
+    fn test_is_contains_same_digits() {
+        assert_eq!(true, is_contains_same_digits(123, 321));
+        assert_eq!(false, is_contains_same_digits(123, 3211));
+        assert_eq!(false, is_contains_same_digits(123, 3210));
+    }
+}
