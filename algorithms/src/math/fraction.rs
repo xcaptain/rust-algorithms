c78f702999268bc53272882b823f35e0a85a6490
Joey <joey.xf@gmail.com>
2019-01-24 11:59:53 +0800

    add basic implementation of my fraction type

--- algorithms/src/math/fraction.rs
+++ algorithms/src/math/fraction.rs
@@ -1,25 +1,21 @@
-use crate::math::lcm::lcm2;
-use num_bigint::{BigInt, ToBigInt};
+use num_integer::Integer;
 
 #[derive(Clone, Debug)]
-pub struct Fraction {
-    pub numerator: BigInt,
-    pub denominator: BigInt,
+pub struct Fraction<T: Integer + Clone> {
+    pub numerator: T,
+    pub denominator: T,
 }
 
-impl Fraction {
-    pub fn new(numerator: BigInt, denominator: BigInt) -> Self {
+impl<T: Integer + Clone> Fraction<T> {
+    pub fn new(numerator: T, denominator: T) -> Self {
         Fraction {
             numerator,
             denominator,
         }
     }
 
-    pub fn add(&self, adder: &Fraction) -> Fraction {
-        let new_deno = lcm2(
-            self.denominator.to_bigint().unwrap(),
-            adder.denominator.to_bigint().unwrap(),
-        );
+    pub fn add(&self, adder: &Fraction<T>) -> Fraction<T> {
+        let new_deno = lcm(self.denominator.clone(), adder.denominator.clone());
         let new_self_num = (new_deno.clone() / self.denominator.clone()) * self.numerator.clone();
         let new_adder_num =
             (new_deno.clone() / adder.denominator.clone()) * adder.numerator.clone();
@@ -30,7 +26,7 @@ impl Fraction {
         }
     }
 
-    pub fn reciprocal(&self) -> Fraction {
+    pub fn reciprocal(&self) -> Fraction<T> {
         Fraction {
             numerator: self.denominator.clone(),
             denominator: self.numerator.clone(),
@@ -38,12 +34,37 @@ impl Fraction {
     }
 }
 
+pub fn lcm<T: Integer + Clone>(m: T, n: T) -> T {
+    let g = gcd(m.clone(), n.clone());
+    return m * n / g;
+}
+
+pub fn gcd<T: Integer + Clone>(mm: T, nn: T) -> T {
+    let mut m = mm;
+    let mut n = nn;
+    while n != T::zero() {
+        let t = n.clone();
+        n = m % n;
+        m = t;
+    }
+    return m;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use num_bigint::BigInt;
 
     #[test]
     fn test_clone() {
+        let f1 = Fraction::new(1, 2);
+        let f2 = f1.clone();
+
+        assert_eq!(1, f2.numerator);
+    }
+
+    #[test]
+    fn test_big_int_clone() {
         let f1 = Fraction::new(BigInt::from(1), BigInt::from(2));
         let f2 = f1.clone();
 
@@ -51,11 +72,20 @@ mod tests {
     }
 
     #[test]
-    fn test_add() {
+    fn test_big_int_add() {
         let f1 = Fraction::new(BigInt::from(1), BigInt::from(2));
         let f2 = f1.add(&f1);
 
         assert_eq!(BigInt::from(2), f2.numerator);
         assert_eq!(BigInt::from(2), f2.denominator);
+    }
+
+    #[test]
+    fn test_add() {
+        let f1 = Fraction::new(1, 2);
+        let f2 = f1.add(&f1);
+
+        assert_eq!(2, f2.numerator);
+        assert_eq!(2, f2.denominator);
     }
 }
