ff355597272da1292d0d82c4832fe66a41a56fd6
Joey <joey.xf@gmail.com>
2019-01-30 12:20:58 +0800

    add solution p14 of leetcode

---
+++ leetcode/src/p14.rs
@@ -0,0 +1,60 @@
+pub fn longest_common_prefix(strs: Vec<String>) -> String {
+    let mut pre = String::from("");
+    let mut i = 0;
+    if strs.len() == 0 {
+        return pre;
+    }
+    loop {
+        let first_s: Vec<char> = strs[0].chars().collect();
+        if first_s.len() <= i {
+            return pre;
+        }
+        let ic = first_s[i];
+        for s in &strs {
+            let vs: Vec<char> = s.chars().collect();
+            if vs.len() <= i || ic != vs[i] {
+                return pre;
+            }
+        }
+        pre = format!("{}{}", pre, ic);
+        i += 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_longest_common_prefix() {
+        assert_eq!(
+            "fl".to_string(),
+            longest_common_prefix(vec![
+                "flower".to_string(),
+                "flow".to_string(),
+                "flight".to_string()
+            ])
+        );
+
+        assert_eq!(
+            "".to_string(),
+            longest_common_prefix(vec![
+                "dog".to_string(),
+                "racecar".to_string(),
+                "car".to_string()
+            ])
+        );
+
+        assert_eq!("".to_string(), longest_common_prefix(vec![]));
+
+        assert_eq!(
+            "hello".to_string(),
+            longest_common_prefix(vec!["hello".to_string()])
+        );
+
+        assert_eq!(
+            "a".to_string(),
+            longest_common_prefix(vec!["aa".to_string(), "a".to_string()])
+        );
+    }
+}
