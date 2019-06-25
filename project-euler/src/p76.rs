bbfd2f8f8ef4753c5129fa62f8735e1f5ba8c0d5
Joey <joey.xf@gmail.com>
2019-06-25 11:40:13 +0800

    add  a much optimized method for p76

--- project-euler/src/p76.rs
+++ project-euler/src/p76.rs
@@ -1,26 +1,47 @@
 // http://mathworld.wolfram.com/PartitionFunctionP.html
 // must first know the recursion function to compute,
 // the base is math
+
+use std::collections::HashMap;
+
 pub fn solution_of_p76() -> usize {
-    return part(100);
+    return part1(100);
 }
 
-fn part(n: usize) -> usize {
+// simple recursion solution
+fn part1(n: usize) -> usize {
     let mut s = 0;
     for k in 1..n {
-        s += p(n, k);
+        s += p1(n, k);
     }
     return s;
 }
 
-fn p(n: usize, k: usize) -> usize {
+// dynamic programming, using p_cache to store some computed values
+// which can reduce needless recursion
+fn part2(n: usize) -> usize {
+    let mut s = 0;
+    let mut p_cache = HashMap::new();
+    for k in 1..n {
+        if let Some(p_val) = p_cache.get(&(n, k)) {
+            s += p_val;
+        } else {
+            let n_val = p1(n, k);
+            s += n_val;
+            p_cache.insert((n, k), n_val);
+        }
+    }
+    return s;
+}
+
+fn p1(n: usize, k: usize) -> usize {
     if k == 1 {
         return 1;
     }
     if k > n {
         return 0;
     }
-    return p(n - 1, k - 1) + p(n - k, k);
+    return p1(n - 1, k - 1) + p1(n - k, k);
 }
 
 #[cfg(test)]
@@ -30,12 +51,17 @@ mod tests {
     #[test]
     fn test_solution_of_p76() {
         assert_eq!(190569291, solution_of_p76());
+        assert_eq!(190569291, part2(100));
     }
 
     #[test]
     fn test_sum_num() {
-        assert_eq!(4, part(4));
-        assert_eq!(6, part(5));
-        assert_eq!(10, part(6));
+        assert_eq!(4, part1(4));
+        assert_eq!(6, part1(5));
+        assert_eq!(10, part1(6));
+
+        assert_eq!(4, part2(4));
+        assert_eq!(6, part2(5));
+        assert_eq!(10, part2(6));
     }
 }
