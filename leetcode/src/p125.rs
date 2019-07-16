5d6ca80c1a6fc659832782f14cea604d6fe52e4d
Joey <joey.xf@gmail.com>
2019-07-16 18:21:50 +0800

    daily coding, add p125 and p136

---
+++ leetcode/src/p125.rs
@@ -0,0 +1,42 @@
+// https://leetcode-cn.com/problems/valid-palindrome/
+
+pub fn is_palindrome(s: String) -> bool {
+    let s = s.to_ascii_lowercase();
+    let arr: Vec<char> = s.chars().collect();
+    if arr.is_empty() {
+        return true;
+    }
+    let mut i = 0;
+    let mut j = arr.len() - 1;
+    while i < j {
+        if !arr[i].is_ascii_alphanumeric() {
+            i += 1;
+            continue;
+        }
+        if !arr[j].is_ascii_alphanumeric() {
+            j -= 1;
+            continue;
+        }
+        if arr[i] != arr[j] {
+            return false;
+        }
+        i += 1;
+        j -= 1;
+    }
+    return true;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_palindrome() {
+        assert_eq!(
+            true,
+            is_palindrome(String::from("A man, a plan, a canal: Panama"))
+        );
+        assert_eq!(false, is_palindrome(String::from("race a car")));
+        assert_eq!(true, is_palindrome(String::from("")));
+    }
+}
---
+++ leetcode/src/p136.rs
@@ -0,0 +1,27 @@
+// https://leetcode-cn.com/problems/single-number/
+
+use std::collections::HashSet;
+
+pub fn single_number(nums: Vec<i32>) -> i32 {
+    let uniq_nums: HashSet<i32> = nums.iter().cloned().collect();
+    let mut s1 = 0;
+    for x in uniq_nums.iter() {
+        s1 += x;
+    }
+    let mut s2 = 0;
+    for x in nums.iter() {
+        s2 += x;
+    }
+    return 2 * s1 - s2;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_single_number() {
+        assert_eq!(1, single_number(vec![2, 2, 1]));
+        assert_eq!(4, single_number(vec![4, 1, 2, 1, 2]));
+    }
+}
